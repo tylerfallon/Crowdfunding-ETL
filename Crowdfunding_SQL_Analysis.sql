fcf56f600b31a80993e78e87ae4c221ea843c10b
tylerfallon <tylermfallon@gmail.com>
2022-12-01 04:59:11 -0500

    finalized versions of the code

--- Crowdfunding_SQL_Analysis.sql
+++ Crowdfunding_SQL_Analysis.sql
@@ -2,18 +2,17 @@
 -- 1. (2.5 pts)
 -- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
 
+SELECT cf_id, backers_count FROM campaign WHERE outcome = 'live' ORDER BY backers_count DESC;
 
 
 -- 2. (2.5 pts)
 -- Using the "backers" table confirm the results in the first query.
 
-
+SELECT COUNT(cf_id) FROM backers GROUP BY cf_id;
 
 -- 3. (5 pts)
 -- Create a table that has the first and last name, and email address of each contact.
 -- and the amount left to reach the goal for all "live" projects in descending order. 
-
-
 
 
 -- Check the table
